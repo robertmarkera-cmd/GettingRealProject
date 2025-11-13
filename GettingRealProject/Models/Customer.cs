cbdbd98e3af83b98911b3071370a919d5ee75edf
Amin Christiansen <Amin081204@gmail.com>
2025-11-13 14:17:51 +0100

    CustomerRepo begyndt

--- GettingRealProject/Models/Customer.cs
+++ GettingRealProject/Models/Customer.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.RightsManagement;
+using System.Xml.Linq;
 
 namespace GettingRealProject.Models
 {
-    internal class Customer
+    public class Customer
     {
+
+        public string name { get; set; }
+        public string email { get; set; }
+        public int phoneNumber { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public double balance { get; set; }
+
+
+        public Customer(string Name, string Email, int PhoneNumber, string Username, string Password, double Balance)
+        {
+            name = Name;
+            email = Email;
+            phoneNumber = PhoneNumber;
+            username = Username;
+            password = Password;
+            balance = Balance;
+        }
+
+        public Customer(string Name, string Email, int PhoneNumber, string Username, string Password) : this(Name, Email, PhoneNumber, Username, Password, 0)
+        {
+
+        }
+
+        public Customer() : this ("","",0,"","",0 )
+        {
+
+        }
     }
 }
+
---
+++ GettingRealProject/Models/CustomerRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace GettingRealProject.Models
+{
+    public class CustomerRepository
+    {
+        private List<Customer> customers;
+
+        public CustomerRepository()
+        {
+            customers = new List<Customer>();
+            InitializeRepository();
+        }
+
+        private void InitializeRepository()
+        {
+            try
+            {  
+                using (StreamReader sr = new StreamReader("Customers.csv"))
+                {
+                  
+                    string line = sr.ReadLine();
+
+                    while (line != null)
+                    {
+                        string[] parts = line.Split(',');
+
+                        this.Add(parts[0], parts[1], int.Parse(parts[2]), parts[3], parts[4], double.Parse(parts[5]));
+
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+        }
+
+        public Customer Add(string name, string email, int phoneNumber, string username, string password, double balance)
+        {
+
+
+            Customer result = null;
+
+            if (!string.IsNullOrEmpty(name) &&
+                !string.IsNullOrEmpty(email) &&
+                phoneNumber >= 0 &&
+                !string.IsNullOrEmpty(username) &&
+                !string.IsNullOrEmpty(password) &&
+                balance >= 0)
+
+            {
+                result = new Customer();
+                {
+                    Name = name,
+                    Email = email,
+                    PhoneNumber = phoneNumber,
+                    Username = username,
+                    Password = password,
+                    Balance = balance;
+
+                }
+                
+                customers.Add(result);
+            }
+            else
+                throw (new ArgumentException("Not all arguments are valid"));
+
+            return result;
+        }
+    }
+}
+
