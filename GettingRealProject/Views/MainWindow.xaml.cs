83eb81b2b765434997efe550086c4a10956a9e26
Amin Christiansen <Amin081204@gmail.com>
2025-12-01 11:13:42 +0100

    dev branch

--- GettingRealProject/Views/MainWindow.xaml.cs
+++ GettingRealProject/Views/MainWindow.xaml.cs
@@ -2,9 +2,7 @@
 
 namespace GettingRealProject.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
