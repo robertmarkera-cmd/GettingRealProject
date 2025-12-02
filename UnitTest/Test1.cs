2df928f616d485baf8a113cb482e5d4e8d59c819
Amin Christiansen <Amin081204@gmail.com>
2025-12-02 13:58:54 +0100

    første test virker

--- UnitTest/Test1.cs
+++ UnitTest/Test1.cs
@@ -1,18 +1,72 @@
-﻿namespace UnitTest
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GettingRealProject;
+using GettingRealProject.Models;
+
+namespace UnitTest
+
 {
     [TestClass]
     public sealed class Test1
     {
+
+        
+
+        //Customer c1, c2, c3;
+
+
+        [TestInitialize]
+        public void Init()
+        {
+            
+
+        }
+
+
         [TestMethod]
-        public void AddCustomer()
+        public void AddCustomerTrue()
+        {
+            // Arrange
+            var cr = new CustomerRepository();
+            // Act
+            var customer = cr.Add("Jan", "j@gmail.com", 10101010, "jan12", "password", 0);
+
+            // Assert
+            Assert.AreEqual("Jan", customer.Name);
+            Assert.AreEqual("j@gmail.com", customer.Email);
+            Assert.AreEqual(10101010, customer.PhoneNumber);
+            Assert.AreEqual("jan12", customer.UserName);
+            Assert.AreEqual("password", customer.Password);
+            Assert.AreEqual(0, customer.Balance);
+        }
+
+
+        [TestMethod]
+        public void GettingCustomerDetail()
         {
 
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void GettingCustomerBalance()
         {
 
         }
+        [TestMethod]
+        public void UpdatingCustomerBalance()
+        {
+
+        }
+        [TestMethod]
+        public void ReadToStreamrider()
+        {
+
+        }
+
+        [TestMethod]
+        public void WriteToStreamrider()
+        {
+
+        }
+
     }
 }
